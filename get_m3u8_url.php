07321983a79d5229bf85e419bbea5d7fa44339d5
Bishal <138358353+bishal520@users.noreply.github.com>
2024-11-26 21:23:08 +0545

    Update get_m3u8_url.php

--- get_m3u8_url.php
+++ get_m3u8_url.php
@@ -2,7 +2,7 @@
 
 // Check if it's an AJAX call for the m3u8 URL
 if (isset($_GET['_']) && $_GET['_'] === "get_m3u8_url") {
-    $m3u8Url = "https://github.com/aniketda/iptv2050/blob/main/iptv"; // Replace with your actual URL or logic
+    $m3u8Url = "https://streamingengine.rtvplus.tv/rtvpluslive/rtvpluslive.stream/chunklist_w2015195144.m3u8"; // Replace with your actual URL or logic
     // IMPORTANT! Security measures (e.g., authentication) should go here.
     header('Content-Type: application/vnd.apple.mpegurl');
     echo $m3u8Url;
